d7ba74eee1efa3c77b19107b1cba4ec534eae597
Omar Dalgamuni <omar.dalgamuni@better.care>
2022-11-15 11:56:52 +0100

    added admin

--- apps/sandbox/src/app/app.module.ts
+++ apps/sandbox/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+    imports: [BrowserModule, BrowserAnimationsModule,
+      RouterModule.forRoot([
+        { path: 'admin', loadChildren: () =>
+            import('@angular/admin').then(m => m.AdminModule) },
+          { path: 'visitor', loadChildren: () =>
+              import('@angular/visitor').then(m =>
+                m.VisitorModule) },{ path: '', pathMatch: 'full', redirectTo:
+            'tour' }
+      ])],
   providers: [],
   bootstrap: [AppComponent],
 })
